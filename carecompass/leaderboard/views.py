2320e7457dc2d2de49c169b9529dc0cec2806f57
ankitahossain <136112474+ankitahossain@users.noreply.github.com>
2025-09-12 23:10:47 +0600

    leaderboard
    leaderboard , dashboard design

--- carecompass/leaderboard/views.py
+++ carecompass/leaderboard/views.py
@@ -1,3 +1,14 @@
+from django.db.models import Count, Q
 from django.shortcuts import render
+from user.models import CustomUser
 
-# Create your views here.
+def leaderboard(request):
+    top_users = CustomUser.objects.filter(role="user").annotate(count=Count("reports")).order_by("-count")[:10]
+    top_volunteers = CustomUser.objects.filter(role="volunteer").annotate(count=Count("accepted_reports", filter=Q(accepted_reports__status="completed"))).order_by("-count")[:10]
+    top_ngos = CustomUser.objects.filter(role="ngo").annotate(count=Count("accepted_reports", filter=Q(accepted_reports__status="completed"))).order_by("-count")[:10]
+
+    return render(request, "leaderboard.html", {
+        "top_users": top_users,
+        "top_volunteers": top_volunteers,
+        "top_ngos": top_ngos,
+    })
