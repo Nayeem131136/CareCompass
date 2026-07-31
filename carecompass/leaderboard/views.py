33f40d3e59736e0e0a1639eb3168442a8ecd3319
Rupasha khan tandra <22201136@uap-bd.edu>
2026-07-31 22:03:12 +0600

    Add files via upload

--- carecompass/leaderboard/views.py
+++ carecompass/leaderboard/views.py
@@ -1,33 +1,24 @@
 from django.db.models import Count, Q
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from user.models import CustomUser
 
+@login_required
 def leaderboard(request):
-
-    # Top Users – based on submitted reports
-
     top_users = CustomUser.objects.filter(role="user").annotate(
-        count=Count("reports")  # 'reports' হলো Report মডেলের related_name
+        count=Count("reports")
     ).order_by("-count")[:10]
-
-
-    # Top Volunteers – based on completed accepted reports
 
     top_volunteers = CustomUser.objects.filter(role="volunteer").annotate(
         count=Count("accepted_reports", filter=Q(accepted_reports__status="completed"))
     ).order_by("-count")[:10]
 
-
-    # Top NGOs – based on completed accepted reports
-
     top_ngos = CustomUser.objects.filter(role="ngo").annotate(
         count=Count("accepted_reports", filter=Q(accepted_reports__status="completed"))
     ).order_by("-count")[:10]
 
-    context = {
+    return render(request, "leaderboard.html", {
         "top_users": top_users,
         "top_volunteers": top_volunteers,
         "top_ngos": top_ngos,
-    }
-
-    return render(request, "leaderboard.html", context)
+    })
