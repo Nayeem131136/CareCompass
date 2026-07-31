5b7afce96be3e6a3acbeebc038b8d727f2f700ce
Rupasha khan tandra <22201136@uap-bd.edu>
2026-07-31 21:58:48 +0600

    Add files via upload

--- carecompass/report/admin.py
+++ carecompass/report/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import Report
-admin.site.register(Report)
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'created_by', 'accepted_by', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['title', 'description']
+    readonly_fields = ['created_at']
